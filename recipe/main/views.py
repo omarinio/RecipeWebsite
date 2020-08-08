c350eda316c4d296de3ff313c2c1f4d4e54ae46c
Omar Suleiman <jb18940@bristol.ac.uk>
2020-08-08 16:03:55 +0200

    added about page, linked my github

--- recipe/main/views.py
+++ recipe/main/views.py
@@ -18,6 +18,10 @@ def index(request):
     return render(request, "main/index.html") 
 
 
+def about(request):
+    return render(request, "main/about.html") 
+
+
 def login_view(request):
     if request.method == "POST":
 
