01946b38eb6dcb9bedc9399c86df25acaacbee91
Omar Suleiman <jb18940@bristol.ac.uk>
2020-08-08 18:51:21 +0200

    new model for likes

--- recipe/main/models.py
+++ recipe/main/models.py
@@ -16,3 +16,13 @@ class Recipe(models.Model):
 class Follow(models.Model):
     user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followed")
     follower = models.ForeignKey("User", on_delete=models.CASCADE, related_name="followers")
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Commenter")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ListComment")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Liker")
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="LikedRecipe")
